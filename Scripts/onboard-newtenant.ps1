be92c0acc5c9ef395409124d8346d0cf385eed76
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-07-28 23:48:57 -0600

    onboarding fix

--- Scripts/onboard-newtenant.ps1
+++ Scripts/onboard-newtenant.ps1
@@ -1,2 +1,2 @@
-new-azdeployment -name lh-atg-onboarding -location southcentralus `
-  -templateuri 
+new-azsubscriptiondeployment -name lh-atg-onboarding -location southcentralus `
+  -templateuri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/onboarding/LHATGOnboarding.json
