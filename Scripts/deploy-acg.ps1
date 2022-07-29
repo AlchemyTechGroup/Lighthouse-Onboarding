b6d8c3e90feb010fb5268b7ae206e57138a0ad0c
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-07-28 23:28:43 -0600

    add lh

--- Scripts/deploy-acg.ps1
+++ Scripts/deploy-acg.ps1
@@ -1,4 +1,4 @@
-$rgname = "rg-atg-acgallery"
+$rgname = "rg-lh-atg-acgallery"
 new-azresourcegroup -name $rgname -location southcentralus
 New-AzResourceGroupDeployment -Name LH-ATG-AzureComputeGallery  -ResourceGroupName $rgname `
   -TemplateUri          https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/AzureGalleryImages/AzureGalleryDeployment.json `
