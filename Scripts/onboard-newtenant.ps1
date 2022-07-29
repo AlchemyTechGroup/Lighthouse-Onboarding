98c27f025c4c69f6222f36554bf31c02c32589e6
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-07-29 06:46:23 -0600

    tags

--- Scripts/onboard-newtenant.ps1
+++ Scripts/onboard-newtenant.ps1
@@ -5,8 +5,15 @@ new-azsubscriptiondeployment -name lh-atg-onboarding -location $location `
   -TemplateUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/onboarding/LHATGOnboarding.json `
   -customerName "$customerName" -verbose
 
+  
+New-AzDeployment -Name lh-atg-tags -location "southcentralus" `
+  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json"#> `
+  -TemplateFile ..\templates\VMTags\VMTags2.json `
+  -customerName $customerName -verbose
 
-New-AzSubscriptionDeployment -Name lh-atg-tags -Location $location `
+start-sleep(5000)
+  
+New-AzResourceGroupDeployment -Name lh-atg-tags -ResourceGroupName rg-vm-lh-atg-images `
   <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json"#> `
   -TemplateFile ..\templates\VMTags\VMTags.json `
   -customerName $customerName -verbose
