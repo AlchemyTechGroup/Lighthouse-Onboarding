c00866fb3fa59f16b8adaf63ef2d84145fcc91d2
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-07-29 00:08:58 -0600

    onboardingupdate

--- Scripts/onboard-newtenant.ps1
+++ Scripts/onboard-newtenant.ps1
@@ -1,2 +1,10 @@
-new-azsubscriptiondeployment -name lh-atg-onboarding -location southcentralus `
-  -templateuri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/onboarding/LHATGOnboarding.json
+$location = "southcentralus"
+$customerName = "dlind"
+
+new-azsubscriptiondeployment -name lh-atg-onboarding -location $location `
+  -TemplateUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/onboarding/LHATGOnboarding.json `
+  -customerName $customerName -location $location
+
+New-AzSubscriptionDeployment -Name lh-atg-tags -Location $location `
+  -TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json" `
+  -customerName $customerName -location $location
