fb9e0460880cea93f6b466816d9d21c1fa094104
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-07-28 23:13:37 -0600

    updateparam

--- Scripts/deploy-acg.ps1
+++ Scripts/deploy-acg.ps1
@@ -2,4 +2,5 @@ $rgname = "rg-atg-acgallery"
 new-azresourcegroup -name $rgname -location southcentralus
 New-AzResourceGroupDeployment -Name LH-ATG-AzureComputeGallery  -ResourceGroupName $rgname `
   -TemplateUri          https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/AzureGalleryImages/AzureGalleryDeployment.json `
-  -TemplateParameterUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/AzureGalleryImages/AzureGalleryDeployment.parameters.json
+  -TemplateParameterUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/AzureGalleryImages/AzureGalleryDeployment.parameters.prod.json
+  -customerName "dlind"
