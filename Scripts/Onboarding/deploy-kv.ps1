6d5db0b70b271a71e12e1af6b0bef6481b8896d3
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-08-09 13:25:05 -0600

    keyv

--- Scripts/Onboarding/deploy-kv.ps1
+++ Scripts/Onboarding/deploy-kv.ps1
@@ -2,7 +2,7 @@ param (
   [Parameter( Mandatory=$false )]
   [string]$CustomerName
 )
-$rgname = "rg-acg-lh-atg-keyvault"
+$rgname = "rg-kv-lh-atg-keyvault"
 New-AzResourceGroupDeployment -Name LH-ATG-KeyVault  -ResourceGroupName $rgname `
   -TemplateUri          https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/KeyVault/KeyVaultDeployment.json `
   -TemplateParameterUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/KeyVault/KeyVaultDeployment.parameters.json `
--- Scripts/Onboarding/onboard-newtenant.ps1
+++ Scripts/Onboarding/onboard-newtenant.ps1
@@ -5,19 +5,19 @@ new-azsubscriptiondeployment -name lh-atg-onboarding -location $location `
   -TemplateUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/onboarding/LHATGOnboarding.json `
   -customerName "$customerName" -verbose
 
-start-sleep -seconds 10
-  
-New-AzDeployment -Name lh-atg-tags -location "southcentralus" `
-  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json" #> `
-  -TemplateFile ..\..\templates\VMTags\VMTags2.json `
-  -customerName "$customerName" -verbose
+#start-sleep -seconds 10
 
-start-sleep -seconds 10
+#New-AzDeployment -Name lh-atg-tags -location "southcentralus" `
+#  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json" #> `
+#  -TemplateFile ..\..\templates\VMTags\VMTags2.json `
+#  -customerName "$customerName" -verbose
+
+#start-sleep -seconds 10
   
-New-AzResourceGroupDeployment -Name lh-atg-tags -ResourceGroupName rg-vm-lh-atg-images `
-  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json" #> `
-  -TemplateFile ..\..\templates\VMTags\VMTags.json `
-  -customerName "$customerName" -verbose
+#New-AzResourceGroupDeployment -Name lh-atg-tags -ResourceGroupName rg-vm-lh-atg-images `
+#  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json" #> `
+#  -TemplateFile ..\..\templates\VMTags\VMTags.json `
+#  -customerName "$customerName" -verbose
 
 
 #& .\deploy-acg.ps1
