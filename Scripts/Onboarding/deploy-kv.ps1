34353c655b398e59a4ae2072cb8d69976e7d9b26
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-08-09 13:00:10 -0600

    keyvault deploy

--- Scripts/Onboarding/deploy-kv.ps1
+++ Scripts/Onboarding/deploy-kv.ps1
@@ -1,4 +1,9 @@
-$rgname = "rg-acg-lh-atg-computegallery"
-New-AzResourceGroupDeployment -Name LH-ATG-AzureComputeGallery  -ResourceGroupName $rgname `
-  -TemplateUri          https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/AzureGalleryImages/AzureGalleryDeployment.json `
-  -TemplateParameterUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/AzureGalleryImages/AzureGalleryDeployment.parameters.json 
+param (
+  [Parameter( Mandatory=$false )]
+  [string]$CustomerName
+)
+$rgname = "rg-acg-lh-atg-keyvault"
+New-AzResourceGroupDeployment -Name LH-ATG-KeyVault  -ResourceGroupName $rgname `
+  -TemplateUri          https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/KeyVault/KeyVaultDeployment.json `
+  -TemplateParameterUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/KeyVault/KeyVaultDeployment.parameters.json `
+  -customerName $CustomerName
--- Scripts/Onboarding/onboard-newtenant.ps1
+++ Scripts/Onboarding/onboard-newtenant.ps1
@@ -5,6 +5,7 @@ new-azsubscriptiondeployment -name lh-atg-onboarding -location $location `
   -TemplateUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/onboarding/LHATGOnboarding.json `
   -customerName "$customerName" -verbose
 
+start-sleep -seconds 10
   
 New-AzDeployment -Name lh-atg-tags -location "southcentralus" `
   <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json" #> `
@@ -20,3 +21,4 @@ New-AzResourceGroupDeployment -Name lh-atg-tags -ResourceGroupName rg-vm-lh-atg-
 
 
 #& .\deploy-acg.ps1
+#& .\deploy-kv.ps1 -CustomerName $customerName
