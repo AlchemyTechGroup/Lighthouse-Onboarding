d106e747d669ecf698ca50b3e80d7a27458c685f
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-08-09 14:55:43 -0600

    kvdeploy

--- Scripts/Onboarding/deploy-kv.ps1
+++ Scripts/Onboarding/deploy-kv.ps1
@@ -1,9 +1,14 @@
 param (
-  [Parameter( Mandatory=$false )]
+  [Parameter( Mandatory=$true )]
   [string]$CustomerName
 )
 $rgname = "rg-kv-lh-atg-keyvault"
-New-AzResourceGroupDeployment -Name LH-ATG-KeyVault  -ResourceGroupName $rgname `
-  -TemplateUri          https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/KeyVault/KeyVaultDeployment.json `
-  -TemplateParameterUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/KeyVault/KeyVaultDeployment.parameters.json `
-  -customerName $CustomerName
+$paramfile = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/KeyVault/keyVaultDeployment.parameters.json'
+$paramfile.content.gettype()
+
+$paramfile.content | ConvertFrom-Json 
+
+#New-AzResourceGroupDeployment -Name LH-ATG-KeyVault  -ResourceGroupName $rgname `
+#  -TemplateUri          https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/KeyVault/KeyVaultDeployment.json `
+#  -TemplateParameterUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/KeyVault/KeyVaultDeployment.parameters.json `
+#  -verbose
