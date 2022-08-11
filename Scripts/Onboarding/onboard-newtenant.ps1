68cd472743d883d254d277b369d25bf8fde113c7
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-08-11 13:06:57 -0600

    vmtagupdate

--- Scripts/Onboarding/onboard-newtenant.ps1
+++ Scripts/Onboarding/onboard-newtenant.ps1
@@ -5,19 +5,21 @@ $customerName = "dlind"
 #  -TemplateUri https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/onboarding/LHATGOnboarding.json `
 #  -customerName "$customerName" -verbose
 
+#write-output "pausing for 10 seconds"
 #start-sleep -seconds 10
 
-#New-AzDeployment -Name lh-atg-tags -location "southcentralus" `
-#  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json" #> `
-#  -TemplateFile ..\..\templates\VMTags\VMTags2.json `
-#  -customerName "$customerName" -verbose
+New-AzDeployment -Name lh-atg-tags -location "southcentralus" `
+  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTagsCreation.json" #> `
+  -TemplateFile ..\..\templates\VMTags\VMTagsCreation.json `
+  -customerName "$customerName" -verbose
 
-#start-sleep -seconds 10
+write-output "pausing for 10 seconds"
+start-sleep -seconds 10
   
-#New-AzResourceGroupDeployment -Name lh-atg-tags -ResourceGroupName rg-vm-lh-atg-images `
-#  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTags.json" #> `
-#  -TemplateFile ..\..\templates\VMTags\VMTags.json `
-#  -customerName "$customerName" -verbose
+New-AzResourceGroupDeployment -Name lh-atg-tags -ResourceGroupName rg-vm-lh-atg-images `
+  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/VMTags/VMTagsAssignment.json" #> `
+  -TemplateFile ..\..\templates\VMTags\VMTagsAssignment.json `
+  -verbose
 
 
 #& .\deploy-acg.ps1
