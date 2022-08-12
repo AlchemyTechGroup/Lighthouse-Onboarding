23efda6d993bbf0743da6bed412f6afa778f9938
ITDougLind <40447711+ITDougLind@users.noreply.github.com>
2022-08-12 09:26:26 -0600

    lawdeploy

--- Scripts/Onboarding/deploy-law.ps1
+++ Scripts/Onboarding/deploy-law.ps1
@@ -7,13 +7,13 @@ param (
   $rgname = "rg-vm-lh-atg-images"
 
 
-New-AzDeployment -Name lh-atg-law -location "southcentralus" -logAnalytics $logAnalytics -ResourceGroupName $rgname `
-  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/LogAnalytics/omsExtensionPolicy.json" #> `
-  -TemplateFile ..\..\templates\LogAnalytics\omsExtensionPolicy.json `
-  -verbose
+#New-AzDeployment -Name lh-atg-law -location "southcentralus" -logAnalytics $logAnalytics -ResourceGroupName $rgname `
+#  <#-TemplateUri "https://raw.githubusercontent.com/AlchemyTechGroup/Lighthouse-Onboarding/main/Templates/LogAnalytics/omsExtensionPolicy.json" #> `
+#  -TemplateFile ..\..\templates\LogAnalytics\omsExtensionPolicy.json `
+#  -verbose
 
-write-output "pausing for 10 seconds"
-start-sleep -seconds 10
+#write-output "pausing for 10 seconds"
+#start-sleep -seconds 10
 
   
 New-AzResourceGroupDeployment -Name lh-atg-law-deployment -ResourceGroupName $rgname `
